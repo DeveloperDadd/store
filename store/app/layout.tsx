57c2e05378cbc9cadcdb80d53dfd282ed331267c
claytoncritchfield <clayton.critchfield@gmail.com>
2024-02-08 15:49:27 -0500

    Add link text to navbar, with comments about what each link should take to and what those pages should accomplish

--- store/app/layout.tsx
+++ store/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Image from "next/image";
+import Link from 'next/link';
 import { Playfair_Display, Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -24,13 +26,31 @@ export default function RootLayout({
 }>) {
   return (
     <main className={playfair.className}>
+      <Image
+        src="/logo.webp"
+        width={200}
+        height={200}
+        alt="Critchfield Family Market Logo"
+      />
       <nav>
         <ul>
-          <li>Welcome</li>
+          <li>
+            <Link href="#">Shop</Link> {/* Inside here put beef, pork, chicken, grocery, specials, bundles, cart functionality, and checkout 
+            at checkout give the user the option to have the item delivered if within a reasonable distance, if under a certain amount, add a delivery fee  */}
+          </li>
+          <li>
+            <Link href="#">About us</Link> {/* Put the description about how the business started, pictures of the Critchfields and Larry */}
+          </li>
+          <li>
+            <Link href="#">Wholesale</Link> {/* Just put a description here about how to get signed up here for the  */}
+          </li>
+          <li>
+            <Link href="#">Newsletter</Link> {/* Give users a form to fill out to get signed up for weekly email list */}
+          </li>
         </ul>
     </nav>
     <footer>
-
+      {/* Needs to have an address, telephone number, Link icons to social media accounts, FAQ questions */}
     </footer>
     </main>
   );
